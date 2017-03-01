b4aefacbc176bb2a81de26624c8a4959d5ac4b04
ArasB10 <arasbraziunas10@gmail.com>
2017-03-01 20:40:59 +0200

    Add files via upload
    Testing ninja dead animation

--- sketch.js
+++ sketch.js
@@ -1,14 +1,12 @@
-var back;
+var ninja;
 
 function setup() {
   createCanvas(800,400);
-  back = loadAnimation("assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png", "assets/5.png",
-                       "assets/6.png", "assets/7.png", "assets/8.png", "assets/9.png", "assets/Layer_0009_2.png", "assets/Layer_0010_1.png");
+  ninja = loadAnimation("assets/Dead_000","assets/Dead_009");
  // createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  background(255);
-  animation(back, 300, 100);  
+  animation(ninja, 300, 100);  
   //drawSprites();
 }

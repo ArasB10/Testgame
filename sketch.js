cac80c26d693ddf8d6445ac08e92d65f37dfa7c7
ArasB10 <arasbraziunas10@gmail.com>
2017-03-01 09:19:47 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var back;
+
+function setup() {
+  createCanvas(800,400);
+  back = loadAnimation("assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png", "assets/5.png", "assets/6.png", "assets/7.png", "assets/8.png", "assets/9.png");
+ // createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  animation(back, 300, 100);  
+  //drawSprites();
+}

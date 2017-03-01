dd35468ee7d9aebedc39ec672ba9b45adc4c2150
ArasB10 <arasbraziunas10@gmail.com>
2017-03-01 13:10:15 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ var back;
 
 function setup() {
   createCanvas(800,400);
-  frameRate(30);
   back = loadAnimation("assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png", "assets/5.png",
                        "assets/6.png", "assets/7.png", "assets/8.png", "assets/9.png", "assets/Layer_0009_2.png", "assets/Layer_0010_1.png");
  // createSprite(400, 200, 50, 50);

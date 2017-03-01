1dea47229de839271cd14f02350250d02397ac42
ArasB10 <arasbraziunas10@gmail.com>
2017-03-01 21:01:07 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var ninja;
+var mimage;
 
 function setup() {
   createCanvas(800,400);
+  mimage = loadImage("assets/Dead_000.png");
   ninja = loadAnimation("assets/Dead_000.png","assets/Dead_001.png","assets/Dead_002.png","assets/Dead_003.png","assets/Dead_004.png"
                        ,"assets/Dead_005.png","assets/Dead_006.png","assets/Dead_007.png","assets/Dead_008.png","assets/Dead_009.png");
  // createSprite(400, 200, 50, 50);
@@ -10,6 +12,7 @@ function setup() {
 function draw() {
   //background(200);
   text("all good",50,50);
+  image(mimage, 100,100,50,50);
   animation(ninja, 400, 400);  
   //drawSprites();
 }
